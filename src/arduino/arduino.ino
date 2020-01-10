3203752f8146e512dfef82d52dd7c2eee9ae52f0
Andrea Lanzani <lanzani.andrea29@gmail.com>
2020-01-10 15:14:49 +0100

    Updated to WPF && Finished arduino code
    The C# App has been updated to the newer WPF.
    Arduino is now fully functional

--- src/arduino/arduino.ino
+++ src/arduino/arduino.ino
@@ -1,9 +1,50 @@
-void setup()
-{
+#include <MD_TCS230.h>
+#include <FreqCount.h>
 
+// Calibration
+#define DARK_R 3879
+#define DARK_G 2960
+#define DARK_B 3838
+
+#define WHITE_R 44954
+#define WHITE_G 49706
+#define WHITE_B 67180
+
+// Pin definitions
+#define  S2_OUT  12
+#define  S3_OUT  13
+
+MD_TCS230 CS(S2_OUT, S3_OUT);
+
+void setup() {
+  Serial.begin(9600);
+  CS.begin();
+
+  // Calibration
+  sensorData sd;
+
+  // Dark Cal
+  sd.value[0] = DARK_R;
+  sd.value[1] = DARK_G;
+  sd.value[2] = DARK_B;
+  CS.setDarkCal(&sd);
+
+  // White Cal
+  sd.value[0] = WHITE_R;
+  sd.value[1] = WHITE_G;
+  sd.value[2] = WHITE_B;
+  CS.setWhiteCal(&sd);
 }
 
-void loop()
-{
-    
+void loop() {
+  colorData rgb;
+  
+  CS.read();
+  while (!CS.available());
+  
+  CS.getRGB(&rgb);
+  Serial.print("C");
+  Serial.write(rgb.value[TCS230_RGB_R]);
+  Serial.write(rgb.value[TCS230_RGB_G]);
+  Serial.write(rgb.value[TCS230_RGB_B]);
 }
---
+++ src/color-carillon/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace color_carillon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPort serialPort;
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                serialPort = new SerialPort("COM3", 9600);
+                serialPort.Open();
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Whoops! Something went wrong.");
+                Console.WriteLine(ex.Message);
+            }
+            serialPort.DataReceived += SerialPort_DataReceived;
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort.BytesToRead > 0)
+            {
+                char cmd = (char)serialPort.ReadChar();
+                if (cmd.Equals('C'))
+                {
+                    while (serialPort.BytesToRead < 3) ;
+                    int r = (int)serialPort.ReadByte();
+                    int g = (int)serialPort.ReadByte();
+                    int b = (int)serialPort.ReadByte();
+                    Dispatcher.BeginInvoke(new Action(delegate
+                    {
+                        tbR.Text = r.ToString();
+                        tbG.Text = g.ToString();
+                        tbB.Text = b.ToString();
+                    }));
+                }
+            }
+        }
+    }
+}
