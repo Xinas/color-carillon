2267cc4e49ecd24917f9779d539b262b86309c83
Andrea Lanzani <lanzani.andrea29@gmail.com>
2020-01-10 19:27:58 +0100

    Added playback
    The app now plays a sound for each color

--- src/color-carillon/Enums.cs
+++ src/color-carillon/Enums.cs
@@ -8,18 +8,19 @@ namespace color_carillon
 {
     class Enums
     {
+        // TODO: Add more colors and adjust frequencies.
         public enum Color
         {
-            None,
-            Black,
-            White,
-            Gray,
-            Red,
-            Yellow,
-            Green,
-            Cyan,
-            Blue,
-            Magenta
+            None = 20,
+            Black = 100,
+            White = 200,
+            Gray = 300,
+            Red = 400,
+            Yellow = 500,
+            Green = 600,
+            Cyan = 700,
+            Blue = 800,
+            Magenta = 900
         }
     }
 }
--- src/color-carillon/MainWindow.xaml.cs
+++ src/color-carillon/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
@@ -22,6 +24,15 @@ namespace color_carillon
     public partial class MainWindow : Window
     {
         SerialPort serialPort;
+        Enums.Color lastColor = Enums.Color.None;
+        SignalGenerator sine = new SignalGenerator()
+        {
+            Gain = 0.2,
+            Frequency = 20,
+            Type = SignalGeneratorType.Sin
+        };
+        WaveOutEvent @event = new WaveOutEvent();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,6 +46,8 @@ namespace color_carillon
                 Console.WriteLine(ex.Message);
             }
             serialPort.DataReceived += SerialPort_DataReceived;
+
+            @event.Init(sine);
         }
 
         private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -48,53 +61,76 @@ namespace color_carillon
                     int r = serialPort.ReadByte();
                     int g = serialPort.ReadByte();
                     int b = serialPort.ReadByte();
+
+                    Enums.Color detectedColor = Utils.Classify(r, g, b);
+                    PlayIf(detectedColor);
+                    lastColor = detectedColor;
+
                     Dispatcher.BeginInvoke(new Action(delegate
                     {
-                        tbR.Text = r.ToString();
-                        tbG.Text = g.ToString();
-                        tbB.Text = b.ToString();
-                        RectangleColor.Fill = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
-
-                        Enums.Color detectedColor = Utils.Classify(r, g, b);
-                        string detected = "";
-                        switch (detectedColor)
-                        {
-                            case Enums.Color.None:
-                                detected = "None";
-                                break;
-                            case Enums.Color.Black:
-                                detected = "Black";
-                                break;
-                            case Enums.Color.White:
-                                detected = "White";
-                                break;
-                            case Enums.Color.Gray:
-                                detected = "Gray";
-                                break;
-                            case Enums.Color.Red:
-                                detected = "Red";
-                                break;
-                            case Enums.Color.Yellow:
-                                detected = "Yellow";
-                                break;
-                            case Enums.Color.Green:
-                                detected = "Green";
-                                break;
-                            case Enums.Color.Cyan:
-                                detected = "Cyan";
-                                break;
-                            case Enums.Color.Blue:
-                                detected = "Blue";
-                                break;
-                            case Enums.Color.Magenta:
-                                detected = "Magenta";
-                                break;
-                        }
-
-                        tbDetected.Text = detected;
+                        Update(r, g, b);
                     }));
                 }
             }
+        }
+        
+        private void PlayIf(Enums.Color color)
+        {
+            if (lastColor != color)
+            {
+                if (@event.PlaybackState == PlaybackState.Playing)
+                {
+                    @event.Stop();
+                    sine.Frequency = (double)color;
+                    @event.Init(sine);
+                }
+                @event.Play();
+            }
+        }
+
+        public void Update(int r, int g, int b)
+        {
+            tbR.Text = r.ToString();
+            tbG.Text = g.ToString();
+            tbB.Text = b.ToString();
+            RectangleColor.Fill = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+
+            string detected = "";
+            switch (lastColor)
+            {
+                case Enums.Color.None:
+                    detected = "None";
+                    break;
+                case Enums.Color.Black:
+                    detected = "Black";
+                    break;
+                case Enums.Color.White:
+                    detected = "White";
+                    break;
+                case Enums.Color.Gray:
+                    detected = "Gray";
+                    break;
+                case Enums.Color.Red:
+                    detected = "Red";
+                    break;
+                case Enums.Color.Yellow:
+                    detected = "Yellow";
+                    break;
+                case Enums.Color.Green:
+                    detected = "Green";
+                    break;
+                case Enums.Color.Cyan:
+                    detected = "Cyan";
+                    break;
+                case Enums.Color.Blue:
+                    detected = "Blue";
+                    break;
+                case Enums.Color.Magenta:
+                    detected = "Magenta";
+                    break;
+            }
+
+            tbDetected.Text = detected;
         }
     }
 }
